937c9fdb68fe040244025b10f5bb7e17f930ba08
CamilaMarins <CAMILARODRIGUESMARINS07@GMAIL.COM>
2024-09-26 15:41:24 -0300

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,49 @@
+let alt, peso, idade, cont = "sim" , alt_p_jovem , idade_jovem = 0, peso_p_velha, media_idade = 0, nao, idades, idade_velha = 0
+
+    while(cont == "sim"){
+
+    alt = Number(prompt("Digite sua altura:"))
+
+    peso = Number(prompt("Digite seu peso:"))
+
+    idade = Number(prompt("Digite sua idade:"))
+
+    cont = (prompt("Deseja continuar:"))
+   
+    }
+
+    if(cont == "nao"){
+
+        if(idade > idade_jovem){
+
+            idade_jovem = idade
+
+            
+
+            alt > alt_p_jovem
+
+            alt_p_jovem = alt 
+
+            console.log (alt)
+        }
+
+        if(idade > idade_velha){
+
+            peso = peso_p_velha
+
+            peso_p_velha = peso 
+
+            console.log (peso)
+        }
+
+        if(idades > media_idade){
+
+            peso_p_velha = peso 
+
+            console.log ("A média de idades de todas as pessoas:")
+        }
+
+    }
+
+    
+ 
